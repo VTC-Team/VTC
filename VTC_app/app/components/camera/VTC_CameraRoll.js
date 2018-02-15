ab91892de19ea6f065c69bdd8eb94df03f43fbf7
Austin Yen <auyen@ucsc.edu>
2018-02-14 20:46:46 -0800

    New camera roll, can select one photo at a time

--- VTC_app/app/components/camera/VTC_CameraRoll.js
+++ VTC_app/app/components/camera/VTC_CameraRoll.js
@@ -3,20 +3,30 @@
 
 import React, { Component } from 'react';
 import {
-    CameraRoll, Image, StyleSheet, TouchableHighlight, View, AppRegistry
+    CameraRoll, Image, StyleSheet, TouchableHighlight, View, AppRegistry, Modal,  Button, ScrollView, Dimensions
 } from 'react-native';
 import ViewPhotos from './ViewPhotos';
 
+const { width } = Dimensions.get('window')
 
 export default class VTC_CameraRoll extends Component {
 
 
     state = {
-        photoArray: []
+        photoArray: [],
+        modalVisible: false,
+        index: null
+    }
+
+    setIndex = (index) => {
+        if(index === this.state.index){
+            index = null
+        }
+        this.setState({ index })
     }
 
     getPhotosFromGallery() {
-        CameraRoll.getPhotos({ first: 1000000 })
+        CameraRoll.getPhotos({ first: 20})
             .then(res => {
                 let photoArray = res.edges;
                 this.setState({ photoArray: photoArray })
@@ -26,9 +36,30 @@ export default class VTC_CameraRoll extends Component {
     render() {
         this.getPhotosFromGallery();
         return(
-            
-            <ViewPhotos
-                    photoArray={this.state.photoArray} />
+           
+            <ScrollView
+                contentContainerStyle={styles.scrollView}>
+                {
+                    this.state.photoArray.map((p, i) => {
+                        return(
+                            <TouchableHighlight
+                                style={{opacity: i === this.state.index ? 0.5 : 1}}
+                                key={i}
+                                underlayColor='transparent'
+                                onPress={() => this.setIndex(i)}
+                            >
+                                <Image
+                                    style={{
+                                        width: width/3,
+                                        height: width/3
+                                    }}
+                                    source={{uri:p.node.image.uri}}
+                                />
+                            </TouchableHighlight>
+                        )
+                    })
+                }
+            </ScrollView>
         );
     }
 
@@ -44,6 +75,10 @@ const styles = StyleSheet.create({
     add: {
         width: 100,
         height: 100
+    },
+    scrollView: {
+        flexWrap: 'wrap',
+        flexDirection: 'row'
     }
 });
 
